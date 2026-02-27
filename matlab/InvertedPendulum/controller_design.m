7825f78c460e949d016c7a3033e879381b1f0148
Jojo Liu <covenanter32@gmail.com>
2026-02-27 00:10:49 -0500

    controller design for new 5 state model

--- matlab/InvertedPendulum/controller_design.m
+++ matlab/InvertedPendulum/controller_design.m
@@ -103,4 +103,87 @@ plot(t, step_cl);
 hold off;
 
 % step info
-step = stepinfo(step_cl, t);
+step_info = stepinfo(step_cl, t);
+
+%% Linearization of 5 State Model w/ Integrated position error
+
+M = 0.5; % cart mass (kg)
+m = 0.2; % pendulum mass (kg)
+l = 0.3; % distance from pivot to pendulum COM (m)
+I = 1/12 * m * l^2; % mass moment of inertia about pendulum COM (kg*m^2)
+g = 9.8; % gravitational acceleration m/s^2
+
+% elements of A and B
+a = (m^2*l^2*g) / (I*(m+M) + m*M*l^2);
+b = m*g*l*(m+M) / (I*(m+M) + m*M*l^2);
+c = (I+m*l^2) / (I*(m+M) + m*M*l^2);
+d = m*l / (I*(m+M) + m*M*l^2);
+
+% state matrix
+A = [0 1 0 0;
+     0 0 a 0;
+     0 0 0 1;
+     0 0 b 0];
+
+% input vector
+B = [0; c; 0; d];
+
+A = [A zeros(size(A, 1), 1); -1 zeros(1, size(A, 1))];
+B = [B; 0];
+
+% output matrix
+C = eye(5);
+
+% feedforward
+D = 0;
+
+% create free-response state space model
+sys_ol = ss(A, B, C, D);
+
+% verify controllability of the system
+controllability_matrix = ctrb(A, B);
+rank_controllability = rank(controllability_matrix);
+
+if rank_controllability == length(B)
+    fprintf('System is controllable (rank = %d).\n', rank_controllability);
+else
+    fprintf('System is not controllable (rank = %d).\n', rank_controllability);
+end
+
+% verify observability of the system
+observability_matrix = obsv(A, C);
+rank_observability = rank(observability_matrix);
+
+if rank_observability == length(B)
+    fprintf('System is observable (rank = %d).\n', rank_observability);
+else
+    fprintf('System is not observable (rank = %d).\n', rank_observability);
+end
+
+%% Pole placement for 5 state system
+
+desired_poles = [-2.5+1i, -2.5-1i, -3+0.75i, -3-0.75i, -4];
+K = place(A, B, desired_poles);
+A_cl = A-B*K;
+
+sys_cl = ss(A_cl, B, C, D);
+
+figure();
+hold on;
+pzplot(sys_cl);
+grid on;
+title('Pole-Zero Plot for Open Loop System');
+hold off;
+
+% closed-loop impulse response
+[step_cl, t] = step(sys_cl);
+
+figure();
+hold on;
+grid on;
+legend;
+plot(t, step_cl);
+hold off;
+
+% step info
+step_info = stepinfo(step_cl, t);
